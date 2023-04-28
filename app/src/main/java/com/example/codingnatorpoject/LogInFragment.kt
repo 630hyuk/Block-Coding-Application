2a0bfe65a1f7931c66c994bc2bdf05ae49ef78b1
JWMin556 <117150131+JWMin556@users.noreply.github.com>
2023-04-28 19:49:34 +0900

    로그인 화면을 추가했습니다

--- app/src/main/java/com/example/codingnatorpoject/LogInFragment.kt
+++ app/src/main/java/com/example/codingnatorpoject/LogInFragment.kt
@@ -1,40 +1,34 @@
 package com.example.codingnatorpoject
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.codingnatorpoject.databinding.FragmentEntryBinding
+import com.example.codingnatorpoject.databinding.FragmentLogInBinding
 
-class EntryFragment : Fragment() {
+class LogInFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
         }
     }
 
-    var binding: FragmentEntryBinding? = null
+    var binding: FragmentLogInBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentEntryBinding.inflate(inflater)
+        binding = FragmentLogInBinding.inflate(inflater)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mainIntent = Intent(requireActivity(), MainActivity::class.java) //메인 엑티비티로 화면 전환
 
-        binding?.btnGoToMain?.setOnClickListener {
-            startActivity(mainIntent)
-        }
-
-        binding?.btnLogOut?.setOnClickListener {
-            findNavController().navigate(R.id.action_entryFragment_to_logInFragment)
+        binding?.btnLogIn?.setOnClickListener {
+            findNavController().navigate(R.id.action_logInFragment_to_entryFragment)
         }
     }
 }
