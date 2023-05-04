92eb91b3eb49f07eba3b21dd07672c63f98ca7d6
Pumpkinjam <rnjsl1212@naver.com>
2023-05-05 08:44:42 +0900

    add: signUp fragment

--- app/src/main/java/com/example/codingnatorpoject/LogInFragment.kt
+++ app/src/main/java/com/example/codingnatorpoject/LogInFragment.kt
@@ -1,14 +1,21 @@
 package com.example.codingnatorpoject
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.example.codingnatorpoject.DBConnection.Cognito
 import com.example.codingnatorpoject.databinding.FragmentLogInBinding
 
 class LogInFragment : Fragment() {
+
+    var email = "";
+    var pw = "";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -21,13 +28,46 @@ class LogInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLogInBinding.inflate(inflater)
+
+        // input field에 대한 Listener 추가
+        with (binding) {
+            this!!.inputEmail.addTextChangedListener(object: TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    email = s.toString()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+
+            })
+            this!!.inputPassword.addTextChangedListener(object: TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    pw = s.toString()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+
+            })
+        }
+
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding?.txtSignUp?.setOnClickListener {
+            Log.i("LoginFragment", "Let's Sign up...")
+            //findNavController().navigate(R.id.)
+        }
+
         binding?.btnLogIn?.setOnClickListener {
+            Log.i("Cognito", "Trying login...")
+            val authentication = Cognito(activity?.applicationContext)
+            authentication.userLogin(email, pw)
             findNavController().navigate(R.id.action_logInFragment_to_entryFragment)
         }
     }
